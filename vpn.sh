2a6df466f99c32858efbe8396803a16ccb8d7f50
mehradsadeghi <2012.linkinpark@gmail.com>
2020-01-03 23:37:01 +0330

    Minor improvements and code refactoring. Multiple credentials added.

--- vpn.sh
+++ vpn.sh
@@ -1,90 +1,114 @@
 #!/bin/bash
 
-# Path variables
 PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+PID='/var/run/vpn.pid'
+LOG_PATH='/tmp/vpn_status.txt'
+INFO="Usage: $(basename "$0") (start|stop|status|restart)"
 
-# VPN Variables
+# format: 'host username password cert'. cert (certificate) is not mandatory
+CREDENTIALS=(
+    'host1 username1 password1 cert1'
+    'host2 username2 password2'
+    'host3 username3 password3 cert3'
+)
 
-VPN_HOST='host'
-VPN_USER='username'
-VPN_PASS='password'
-VPN_CERT='' # if not needed leave it as empty string
 
-IFACE='sslinterface'
-PID='/var/run/openconnect.pid'
-TEMP_LOG='/tmp/vpn_status.txt'
-INFO="
-Usage: $(basename "$0") (start|stop|status|restart)
-"
+function start() { 
 
-# Connect to Cisco SSL VPN using passwords from stdin (passed by VPN_PASS variable created prior)
-function connect_vpn() { 
-
-    if [ -s $PID ]
-        then
-            printf "Openconnect is already running\n"
+    if ! is_network_available
+        then 
+            printf "Network is not available. Check your internet connection \n"
             exit 1
-        else
-
-            # checking if ssl certificate is provided
-			if [ -z $VPN_CERT ] 
-                then
-				    CERT=''
-			    else
-				    CERT="--servercert $VPN_CERT"
-			fi
-
-            echo ${VPN_PASS} | openconnect ${VPN_HOST} --user=${VPN_USER} ${CERT} -b --no-dtls --passwd-on-stdin --pid-file $PID > $TEMP_LOG 2>&1
-            if $(grep -iq "connected as" $TEMP_LOG)
-                then
-                    touch $PID
-                    printf "Openconnect started!\n"
-                else
-                    printf "Openconnect failed to start!\n"
-                    cat $TEMP_LOG
-                    exit 2
-            fi
     fi
-}
 
-# Check if openconnect is running through PID file
-function check_openconnect() {
-
-    if [ -s $PID ]
+    if is_vpn_running
         then
-            printf "Openconnect is running!\n"
+            printf "VPN is already running\n"
+            exit 1
+    fi
+
+    for item in "${CREDENTIALS[@]}"; do 
+
+        local credentials=($item)
+        local host=${credentials[0]}
+        local username=${credentials[1]}
+        local password=${credentials[2]}
+        local cert=${credentials[3]}
+
+        echo "Connecting to $host"
+
+        connect $host $username $password $cert
+
+        if is_vpn_running
+            then 
+                printf "VPN is connected \n"
+                print_current_ip_address
+                break
+            else
+                printf "VPN failed to connect! \n"
+        fi
+    done
+}
+
+function connect() {
+    cert=$(get_cert_if_provided $4)
+    echo $3 | openconnect $1 --user=$2 ${cert} -b --no-dtls --passwd-on-stdin --pid-file $PID > $LOG_PATH 2>&1
+}
+
+function status() {
+
+    if is_vpn_running
+        then
+            printf "VPN is running \n"
         else
-            printf "Openconnect is stopped\n"
+            printf "VPN is stopped \n"
     fi
 }
 
-# Confirm if PID file exists, then kill it immediately
-function kill_openconnect() {
+function stop() {
 
-    if [ -s $PID ]
+    if is_vpn_running
         then
             rm -f $PID > /dev/null 2>&1
             kill -9 $(pgrep openconnect) > /dev/null 2>&1
-        else
-            printf "Openconnect is not running!\n"
     fi
+    
+    printf "VPN is disconnected \n"
+    print_current_ip_address
+}
+
+function is_network_available() {
+    ping -q -c 1 -W 1 8.8.8.8 > /dev/null 2>&1;
+}
+
+function is_vpn_running() {
+    test -s $PID 
+}
+
+function print_current_ip_address() {
+    local NEW_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
+    printf "Your IP address is $NEW_IP \n"
+}
+
+function get_cert_if_provided() {
+    test -z $1 && echo '' || echo "--servercert $1"
 }
 
 case "$1" in
 
 	start)
 	
-		connect_vpn
+		start
 		;;
 	
 	stop)
 	
-		kill_openconnect
+		stop
 		;;
 	
 	status)
 	
-		check_openconnect
+		status
 		;;
 	
 	restart)
